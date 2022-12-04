3c9d39279e9aaca4fea0765eb5b6480be5c7657e
Alexander Zaidlin <alex.zaidlin@t-a9.com>
2022-12-04 14:33:07 +0200

    Build Data layer in Catalog API Connect to Mongo

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSetting:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSetting:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSetting:CollectionName"));
+
+            //CatalogContextSeet.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; } 
+    }
+}
