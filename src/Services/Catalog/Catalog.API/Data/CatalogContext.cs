9452fcf6502846ac0a2067d33d6754e065469493
Alexander Zaidlin <alex.zaidlin@t-a9.com>
2022-12-11 12:14:09 +0200

    Finish Catalog API

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -7,12 +7,12 @@ namespace Catalog.API.Data
     {
         public CatalogContext(IConfiguration configuration)
         {
-            var client = new MongoClient(configuration.GetValue<string>("DatabaseSetting:ConnectionString"));
-            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSetting:DatabaseName"));
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
-            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSetting:CollectionName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
 
-            //CatalogContextSeet.SeedData(Products);
+            CatalogContextSeed.SeedData(Products);
         }
 
         public IMongoCollection<Product> Products { get; } 
