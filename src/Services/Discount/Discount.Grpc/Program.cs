a8024bd1b693781bdae64a2b7486725ebb4a7185
Alexander Zaidlin <alex.zaidlin@t-a9.com>
2022-12-18 17:29:57 +0200

    Finish developing Discount Grpc serivce

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,5 @@
 using Discount.API.Repositories;
+using Discount.Grpc.Services;
 //using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,12 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IDiscountRepository,DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
